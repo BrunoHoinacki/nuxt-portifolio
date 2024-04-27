d229a87a2e1ca34e4ea0a9a5793493af212c60a2
Bruno Hoinacki <brunohoinackib@gmail.com>
2024-04-27 00:18:19 -0300

    [Feature] Criando composables
    Funcao de formatar data global useFormatacao

--- components/video/Detalhes.vue
+++ components/video/Detalhes.vue
@@ -1,6 +1,16 @@
+<script setup lang="ts">
+const {diasPassados, diferencaEmDias} = useFormatacao()
+
+const dataVideo = new Date(2023, 3, 27);
+
+diasPassados(dataVideo)
+
+</script>
+
 <template>
     <div>
         <h1>Components -nuxt3</h1>
         <p>Bruno Hoinacki</p>
+        <p>{{ diferencaEmDias }} dias atrás</p>
     </div>
 </template>
